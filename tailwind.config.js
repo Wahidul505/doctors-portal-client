cfb762e78928e04daa809061610fcf27b8ec3c23
Wahidul30 <wahidul505@gmail.com>
2022-05-09 22:20:57 +0600

    created navbar as shared component and added banner in home

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+          primary: "#0FCFEC",
+          secondary:"#19D3AE",
+          accent: "#3A4256",
+          "primary-focus": "orange",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 }
