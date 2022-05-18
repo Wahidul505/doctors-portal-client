eb0520e3184eb89de9a6f1a076b21dad8b5a41a8
Wahidul30 <wahidul505@gmail.com>
2022-05-18 14:23:55 +0600

    created steps note of payment

--- src/__payment_steps.js
+++ src/__payment_steps.js
@@ -10,6 +10,8 @@
  * 8. Install strip in server side.
  * 9. Get payment secret key from stripe dashboard and store it in backend .env file.
  * 10. Require strip and Create an api post method in backend to post payment indent and send booking information to it.
- * 11. In client side > CheckoutForm create fetch with post method and send booking information to payment indent api.
- * 12. 
+ * 11. In client side > CheckoutForm, create fetch with post method and send booking information to payment indent api.
+ * 12. In client side > CheckoutForm > handleSubmit, confirm the payment with confirmCardPayment() and provide payment info in it.
+ * 13. Check the intentError, if exist than return; from the function if not than consider it ass successful payment than display the transaction id to the user.
+ * 14.
 */
